c21d6e5a20c9947fe39f9ef64dcee0a7136b755e
Jensen Ruud <jensen.is.testing@gmail.com>
2022-06-23 16:30:34 -0500

    Created object with state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,28 @@
 import React from "react";
+import memesData from "../data/memesData";
 
 export default function Meme() {
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    memeImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
+  function getMemeImage() {
+    let memeList = allMemeImages.data.memes;
+    let memeNumber = Math.floor(Math.random() * memeList.length);
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        memeImage: memeList[memeNumber].url,
+      };
+    });
+  }
+
   return (
     <div className="meme--container">
-      <form>
+      <div className="meme--form">
         <input
           className="meme--first_text"
           type="text"
@@ -14,8 +33,11 @@ export default function Meme() {
           type="text"
           placeholder="Bottom text"
         ></input>
-        <button className="meme--button">Get a new meme image 🖼</button>
-      </form>
+        <button className="meme--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <img className="meme--image" src={meme.memeImage} />
     </div>
   );
 }
